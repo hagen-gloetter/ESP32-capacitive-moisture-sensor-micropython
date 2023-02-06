c11e9823ebfcfa3290f5dfb00da300efe57bdbae
Hagen Glötter <github@dg7shg.de>
2023-02-06 21:51:59 +0100

    cleanup

--- code/boot.py
+++ code/boot.py
@@ -1,10 +1,7 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-import get_wifi_connection
-import machine
 import os
 import time
 import gc
-from machine import Pin
 import esp
 esp.osdebug(None)
 # esp.osdebug(None)
@@ -19,3 +16,4 @@ print("boot.py")
 #(wifistatus, wifinetwork, ipaddress) = get_wifi_connection.connect_wifi()
 
 print("boot.py done")
+
